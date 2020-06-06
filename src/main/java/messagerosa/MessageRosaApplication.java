57462b6fae7b62a8b4f19d02f1fda1bd8b5b87f2
13218576 <a13218576@LT13218576.local>
2020-06-06 12:13:58 +0530

    Initial Commit

---
+++ src/main/java/messagerosa/MessageRosaApplication.java
@@ -0,0 +1,13 @@
+package messagerosa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageRosaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageRosaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/messagerosa/MessageRosaApplicationTests.java
@@ -0,0 +1,13 @@
+package messagerosa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageRosaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
