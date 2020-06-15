308b9989551bc796ce9b66934615e5689b40533a
Vishal.Singla <vsingla160@gmail.com>
2020-06-16 00:47:56 +0530

    builder

--- src/main/java/messagerosa/core/model/XMessageThread.java
+++ src/main/java/messagerosa/core/model/XMessageThread.java
@@ -2,13 +2,11 @@ package messagerosa.core.model;
 
 import com.sun.istack.NotNull;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class XMessageThread {
