9a8544da0b6e43997afc4a1ed33204e26eb38f81
umangbhola <umangbhola@samagragovernance.in>
2020-07-03 01:35:16 +0530

    Push Changes in message rosa for campaign consumer

--- src/main/java/messagerosa/core/model/CampaignStage.java
+++ src/main/java/messagerosa/core/model/CampaignStage.java
@@ -1,12 +1,21 @@
 package messagerosa.core.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
 public class CampaignStage {
     public enum State {
         STARTING,
         ONGOING,
         COMPLETED
     }
-    String stage;
+    int stage;
     State state;
 
 }
--- src/main/java/messagerosa/core/model/Transformer.java
+++ src/main/java/messagerosa/core/model/Transformer.java
@@ -5,10 +5,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.HashMap;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Transformer {
     private String id;
+    private HashMap<String, String> metaData;
 }
