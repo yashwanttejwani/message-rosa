1bd8e8ea4e62e13e42fe77589bba63b8930cc243
Surabhi <surabhi.mahawar@aurigait.com>
2022-03-15 14:43:49 +0530

    XMsg payload for location & media

--- src/main/java/messagerosa/core/model/MessageMedia.java
+++ src/main/java/messagerosa/core/model/MessageMedia.java
@@ -10,7 +10,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MessageMedia {
-    private String category; //category list {image, audio, document, video}
+    private MediaCategory category; //category list {image, audio, document, video}
     private String text; //caption, if applicable
     private String url;
 }
