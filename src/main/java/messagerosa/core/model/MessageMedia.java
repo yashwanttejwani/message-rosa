fa6f1a01d448b4646c6138c4ccefa7c23577ae39
Yashwant <yashwant.tejwani@aurigait.com>
2022-03-31 18:07:34 +0530

    fixes : QuickReplyBtn File Greater than 5MB

--- src/main/java/messagerosa/core/model/MessageMedia.java
+++ src/main/java/messagerosa/core/model/MessageMedia.java
@@ -13,4 +13,6 @@ public class MessageMedia {
     private MediaCategory category; //category list {image, audio, document, video}
     private String text; //caption, if applicable
     private String url;
+    private MessageMediaError messageMediaError;
+    private Double size;
 }
